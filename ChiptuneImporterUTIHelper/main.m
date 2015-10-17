ef1f3f374decffc4a72c51896f8c168c15a8424a
Daniele Cattaneo <daniele.cattaneo@alice.it>
2015-10-17 23:02:37 +0200

    Fix launchservices not importing UTI declarations from random bundles anymore.
    I simply added a dummy app inside the importer bundle, just for declaring the UTIs.

---
+++ ChiptuneImporterUTIHelper/main.m
@@ -0,0 +1,12 @@
+//
+//  main.m
+//  ChiptuneImporterUTIHelper
+//
+//  Created by Daniele Cattaneo on 17/10/15.
+//  Copyright © 2015 Daniele Cattaneo. All rights reserved.
+//
+
+
+int main(int argc, const char * argv[]) {
+  return 0;
+}

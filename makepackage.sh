daa7b6537872e9fb59d271d08a9f1cfecf1298b3
Daniele Cattaneo <daniele.cattaneo@alice.it>
2015-10-18 17:51:51 +0200

    Add a script for making a pkg which installs the importer.

---
+++ makepackage.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Script to make an installable package of the plugin.
+#
+# Uses xcodebuild, pkgbuild and productbuild.
+#
+
+# Create a clean install directory...
+if test -d build/Package; then
+	sudo chmod -R u+w build/Package
+	sudo rm -rf build/Package
+fi
+mkdir -p build/Package/Root
+mkdir -p build/Package/Resources
+
+# Install into this directory...
+xcodebuild -project "$PWD/ChiptuneImporter.xcodeproj" \
+           -target ChiptuneImporter \
+           -configuration Release \
+           install \
+           DSTROOT="$PWD/build/Package/Root"
+
+# Extract the version number from the project...
+ver=$(/usr/libexec/PlistBuddy -c "Print:CFBundleShortVersionString" "ChiptuneImporter/ChiptuneImporter-Info.plist")
+
+# Make the package with pkgbuild and the product distribution with productbuild...
+echo pkgbuild...
+pkgbuild --identifier       com.danielecattaneo.chiptuneimporter   \
+         --version          "$ver"   \
+         --root             build/Package/Root   \
+         --scripts          ./PackageResources/Scripts \
+         "./ChiptuneImporter.pkg"   
+productbuild --distribution     ./PackageResources/Distribution.xml   \
+             --resources        ./PackageResources/Resources \
+             --package-path     ./ \
+             "./ChiptuneImporter-$ver.pkg"
+rm ./ChiptuneImporter.pkg
